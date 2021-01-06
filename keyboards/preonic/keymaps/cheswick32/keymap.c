2b60c3f89dd01c23d56d7af5991a094da7c4515c
Cheswick32 <sh33ples@gmail.com>
2021-01-06 12:58:45 -0500

    Added shift/caps lock tap dance. Needs testing (caps vs locking caps may change things

--- keyboards/preonic/keymaps/cheswick32/keymap.c
+++ keyboards/preonic/keymaps/cheswick32/keymap.c
@@ -28,11 +28,11 @@ enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  TD_CAPS
+  S_CAPS
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_CAPS] = ACTION_TAP_DANCE_FN(td_caps),
+  [S_CAPS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_caps, td_caps_reset),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | tdcap|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
@@ -54,10 +54,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC__MUTE,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
-),
-
+  TD(S_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, 
+  BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT), 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  V-  |
@@ -257,5 +255,14 @@ bool music_mask_user(uint16_t keycode) {
 
 void td_caps(qk_tap_dance_state_t *state, void *user_data) {
   if (state->count = 5) {
-    register_code16(KC_CAPS);
+    register_code(KC_CAPS);
+  }else{
+    register_code(KS_LSFT);
+  };
+
+void td_caps_reset(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count = 5) {
+    unregister_code(KC_CAPS);
+  }else{
+    register_code(KS_LSFT);
   };
